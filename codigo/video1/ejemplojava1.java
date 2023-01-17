e480ca46a2e4982592428224169da228e65b822f
Hilmer Palomares <45982445+hilmero@users.noreply.github.com>
2023-01-17 16:41:24 -0400

    Update ejemplojava1.java

--- codigo/video1/ejemplojava1.java
+++ codigo/video1/ejemplojava1.java
@@ -1,1 +1,12 @@
 //* Codigo usado como ejemplo en el video #1   *//
+
+public class MiClase {
+  
+  public static void main(String args[]) {
+    
+	  int y=2023;
+	  System.out.println("Hola Mundo, estamos en el "+y);
+    
+  }
+  
+}
