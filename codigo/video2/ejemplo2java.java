d808763a555d4d6fda3d82dc50a78c22fe34ff96
Hilmer Palomares <45982445+hilmero@users.noreply.github.com>
2023-01-17 16:48:15 -0400

    Update ejemplo2java.java
    Ejemplos de tipo de datos básicos en Java

--- codigo/video2/ejemplo2java.java
+++ codigo/video2/ejemplo2java.java
@@ -1,2 +1,50 @@
 /* Codigo de ejemplo de los tipos de datos en Java */
 
+public class MiClase2 {
+  public static void main(String args[]) {
+
+	  System.out.println("\nTipos de datos basicos en JAVA");
+
+//  Tipo de datos byte  
+    byte b=1;
+    System.out.println("Valor de b (byte) "+b);
+
+//  Tipo de datos short 
+    short s=2;
+	  System.out.println("Valor de s (short) "+s);
+
+//  Tipo de datos int 
+    int i=3;
+    int ia=i*2;
+	  System.out.println("Valor de i y ia (int) "+i+" y "+ia);
+
+//  Tipo de datos long  
+    long l=-15444000;
+	  System.out.println("Valor de l (long) "+l);
+
+//  Tipo de datos float 
+    float f=-100f;
+	  System.out.println("Valor de f (float) "+f);
+
+//  Tipo de datos double  
+    double d=100;
+	  System.out.println("Valor de d (double) "+d);
+
+//  Tipo de datos boolean  
+    boolean listo=true;
+	  System.out.println("Valor de listo (boolean) "+listo);
+
+//  Tipo de datos char 
+    char letra1='a';
+    char letra2=97;
+	  System.out.println("Valor de letra1 y letra2 (char) "+letra1+" y "+letra2);
+
+//  Tipo de datos String  
+    String palabra1="cadena de caracteres";
+	  System.out.println("Valor de palabra1 (String) "+palabra1);
+
+//  Uso de Constantes 
+	  final double PI=3.14159;
+	  System.out.println("Valor de PI (double) "+PI);
+  }
+}
