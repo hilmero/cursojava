68c2aa2df82a973c3f52f4790677a629c0d6223a
Hilmer Palomares <45982445+hilmero@users.noreply.github.com>
2023-01-23 22:13:02 -0400

    Update ejemploJava3.java

--- codigo/video3/ejemploJava3.java
+++ codigo/video3/ejemploJava3.java
@@ -1,34 +1,34 @@
 class ejemploJava3 {
 
-	public static void main(String args[]) {
+  public static void main(String args[]) {
   //
   // Operadores aritmeticos
   // 	
-	System.out.println("\nOperadores aritmeticos");
+  System.out.println("\nOperadores aritmeticos");
   double suma, resta, multiplica, divide;
   int resto;
 
   suma=2+2;
   resta=5-2;
-	multiplica=3*3;
-	divide=4/2;
-	resto=5%3;
+  multiplica=3*3;
+  divide=4/2;
+  resto=5%3;
 	
-	System.out.println("suma:"+suma);
-	System.out.println("resta:"+resta);	
-	System.out.println("multiplica:"+multiplica);	
-	System.out.println("divide:"+divide);	
-	System.out.println("resto:"+resto);	
+  System.out.println("suma:"+suma);
+  System.out.println("resta:"+resta);	
+  System.out.println("multiplica:"+multiplica);	
+  System.out.println("divide:"+divide);	
+  System.out.println("resto:"+resto);	
   //
   // Operadores unarios
   // 		
-	int i =3;
-	System.out.println("\nOperadores unarios");
-	System.out.println(i++);
-	System.out.println(i);
-	i=3;
-	System.out.println(++i);
-	System.out.println(i);
+  int i =3;
+  System.out.println("\nOperadores unarios");
+  System.out.println(i++);
+  System.out.println(i);
+  i=3;
+  System.out.println(++i);
+  System.out.println(i);
   //
   // Operadores relacionales
   // 	
@@ -36,22 +36,22 @@ class ejemploJava3 {
   System.out.println(resto!=i);  // true
   System.out.println(5==3);      // false
   System.out.println(5!=3);      // true
-   //
-	// Operadores logicos
-	//
-	System.out.println("\nOperadores logicos");
+  //
+  // Operadores logicos
+  //
+  System.out.println("\nOperadores logicos");
   System.out.println((5 == 3)&&(5 > 3) );    // false
   System.out.println((5 == 3)||(5 > 3) );    // true
-	//
-	// Prioridades de los operadores
-	//
-	System.out.println("\nPrioridad de los Operadores");
-	double y, j=2;
-	y= 4/j+7*3;              // 23
+  //
+  // Prioridades de los operadores
+  //
+  System.out.println("\nPrioridad de los Operadores");
+  double y, j=2;
+  y= 4/j+7*3;              // 23
   System.out.println(y);    
-	y= 4/(j+7)*3;            // 1.3
+  y= 4/(j+7)*3;            // 1.3
   System.out.println(y);   
-	y= 4/++j+7*3; 		 // 22.33
+  y= 4/++j+7*3; 	  // 22.33
   System.out.println(y);   
-	}
+  }
 }
