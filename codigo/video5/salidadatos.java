b25775c4d4b757dd140a8bc7b49e65ca3e448aaa
Hilmer Palomares <45982445+hilmero@users.noreply.github.com>
2023-02-06 15:59:25 -0400

    Create salidadatos.java

---
+++ codigo/video5/salidadatos.java
@@ -0,0 +1,19 @@
+// Ejemplo usado en la video clase no 5
+//
+public class SalidaJava {
+    public static void main(String[] args)     {
+       System.out.printf("%.2f %n", 12.3698);
+       double n = 1.25036;
+       System.out.printf("%.3f %n", n);
+       System.out.printf("%+.3f %n", n);
+       int x = 10;
+       System.out.printf("%d %n", x);
+       System.out.printf("%+d %n", x);
+       System.out.printf("valor de n es %.2f y de x es %d \n", n, x);
+       System.out.printf("n es %+10.2f \n", n);
+       System.out.printf("n es %+010.2f \n", n);
+       System.out.printf("%10s \n","Hola");
+       System.out.printf("%-10s \n","Hola");
+   }
+}
+// eof
