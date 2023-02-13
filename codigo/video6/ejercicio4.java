373e3619705ea7437a4d2c8bf92647ab9ff0437a
Hilmer Palomares <45982445+hilmero@users.noreply.github.com>
2023-02-13 19:44:10 -0400

    Update ejercicio4.java

--- codigo/video6/ejercicio4.java
+++ codigo/video6/ejercicio4.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Ejercicio4 {
    public static void main(String[] args) {
-	 final double PI=3.1416;	
+   final double PI=3.1416;	
 	
-	 // Leemos el radio de la circuferencia
-	 Scanner entrada = new Scanner(System.in);
-	 System.out.println("Introduzca el radio (cm)");
-	 double r = entrada.nextDouble();
+   // Leemos el radio de la circuferencia
+   Scanner entrada = new Scanner(System.in);
+   System.out.println("Introduzca el radio (cm)");
+   double r = entrada.nextDouble();
 	 
-   double area = r * PI;
+   double area = PI * r * r;
  	 
-	 // Muestro el resultado
-	 System.out.printf("El area es %6.2f cm ",area);
-	 }
+   // Muestro el resultado
+   System.out.printf("El area es %6.2f cm ",area);
+   }
 }
