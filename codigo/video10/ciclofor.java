04951b72b8b86fca6d1cbb7ace847edaa4bc0bd8
Hilmer Palomares <45982445+hilmero@users.noreply.github.com>
2023-03-13 17:49:44 -0400

    Create ciclofor.java

---
+++ codigo/video10/ciclofor.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    
+    public static void main(String[] args) {
+       System.out.println("Tabla de multiplicar del 5");
+       for (int i =0 ; i <= 10; i++) {
+          System.out.println(5 + " * " + i + " = " + 5 * i);
+       }
+    }
+}
