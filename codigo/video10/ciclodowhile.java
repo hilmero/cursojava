7e46e57a6dd5ad88ff1b33d9757a67da6862898d
Hilmer Palomares <45982445+hilmero@users.noreply.github.com>
2023-03-13 17:52:14 -0400

    Create ciclodowhile.java

---
+++ codigo/video10/ciclodowhile.java
@@ -0,0 +1,10 @@
+public class ciclowhile {
+    public static void main(String[] args) {
+       System.out.println("Tabla de multiplicar del 5");
+	     int i=0;
+       do {
+          System.out.println(5 + " * " + i + " = " + 5 * i);
+		      i++;
+       } while (i <= 10);
+    }
+}
