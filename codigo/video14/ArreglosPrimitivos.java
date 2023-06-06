34b5324a0816d0b6a8870f73c5aebceb319bf4a2
Hilmer Palomares <45982445+hilmero@users.noreply.github.com>
2023-06-05 23:45:54 -0400

    Create ArreglosPrimitivos.java

---
+++ codigo/video14/ArreglosPrimitivos.java
@@ -0,0 +1,13 @@
+public class ArreglosPrimitivos {
+    public static void main(String args[]) {
+//
+//      Arreglos de tipo de datos primitivos
+//
+        int[] numeros = new int[5];          // Array de enteros
+        double[] precios = new double[3];    // Array de números decimales (double)
+        char[] letras = new char[10];        // Array de caracteres
+        boolean[] banderas = new boolean[2]; // Array de valores booleanos (verdadero/falso)
+//
+        System.out.println("Fin del código");
+    }
+}
